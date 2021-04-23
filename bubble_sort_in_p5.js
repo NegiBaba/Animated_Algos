7e3f3b9bc4b6d1a802a3eb3f17a5d116514638c3
Piyush Negi <48850319+NegiBaba@users.noreply.github.com>
2021-04-23 23:02:26 +0530

    Create bubble_sort_in_p5.js

---
+++ bubble_sort_in_p5.js
@@ -0,0 +1,52 @@
+let bars = [];
+let i,j;
+let bars_width = 2; 
+/* width of a single bar, value is inversly proportional to the number of bars/lines that will be seen on the screen*/
+function setup() {
+  createCanvas(600, 500);
+  
+  bars = new Array(width / bars_width);
+  
+  fill('white');
+  for(i = 0; i < bars.length; i++)
+    {
+      bars[i] = random(height);
+      //console.log(bars[i]);
+    }
+  console.log("Sorting the bars using Bubble ssort");
+  i = 0;
+
+}
+
+function draw() {
+  background(0);
+  if(i > bars.length) 
+    {
+      console.log("Done Sorting");
+      noLoop();
+    } else {
+        for(let j = 0; j < bars.length - 1- i; j++)
+      {
+        let a = bars[j];
+        let b = bars[j + 1];
+        if(a > b)
+          {
+            swap(bars, j, j + 1)
+          }
+      }
+    }
+    i++;
+  
+  stroke('white');
+  fill('white');
+  for(let i = 0; i < bars.length; i++)
+    {
+      rect(i * bars_width, height - bars[i], bars_width, bars[i]);
+    }
+}
+
+function swap(arr, a, b) {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
