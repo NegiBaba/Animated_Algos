40021997e0fa4a440a431373ea1b2d0ec1de71ff
NegiBaba <piyushnegi319@gmail.com>
2021-04-30 22:21:27 +0530

    some changes were made

--- quick_sort_lomuto_partition.py
+++ quick_sort_lomuto_partition.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 #height and width of the window in which animation will run
-WIDTH = 1000
+WIDTH = 1200
 HEIGHT = 600
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -15,18 +15,14 @@ RED = (255, 0, 0)
 # this method will only draw the bars on the given window
 def draw(bars, total_bars, bars_width):
 	for i in range(total_bars):
-		pygame.draw.rect(win, WHITE, (i * bars_width , HEIGHT - bars[i], bars_width, bars[i]))
-
-def draw_sorting(bars, total_bars, bars_width, l, h):
-	for i in range(l, h + 1):
-		pygame.draw.rect(win, RED, (i * 10, HEIGHT - bars[i], bars_width - 2, bars[i]))
+		pygame.draw.rect(win, WHITE, (i * bars_width , HEIGHT - bars[i], bars_width	- 1, bars[i]))
 
 def lomuto_partition(bars, l, h, total_bars, bars_width):
 	pivot = bars[h]
 	i = l - 1
 	j = l
 
-	while(j <= h - 1):
+	while(j <= h):
 		if bars[j] < pivot:
 			i += 1
 			temp = bars[i]
@@ -63,14 +59,14 @@ def quick_sort(bars, l, h, total_bars, bars_width):
 def main():
 
 	# width of single bar displayed, indirectly proportional to number of bars
-	bars_width = 1
+	bars_width = 3
 
 	total_bars = WIDTH // bars_width
 
 	bars = [] # list that will contain the height of the bars
 
 	for i in range(0, total_bars):
-		bars.append(random.randrange(10, 590, 1))
+		bars.append(random.randrange(10, HEIGHT - 5, 1))
 		#randomizing the bars_height from 10 to 590 with 1 as a step
 
 	draw(bars, total_bars, bars_width)
@@ -80,6 +76,9 @@ def main():
 	print(bars)
 
 	quick_sort(bars, 0, total_bars - 1, total_bars, bars_width)
+
+	#time after which the pygame window will be closed autometically
+	pygame.time.delay(5000)
 
 	#printing elemetns after sorting 
 	print(bars)
